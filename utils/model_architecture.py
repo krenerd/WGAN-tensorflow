ce85d53d0342decba3cb91736259961d86e0b052
SieunPark <sieunpark77@gmail.com>
2021-03-08 09:12:24 +0900

    Update model_architecture.py

--- utils/model_architecture.py
+++ utils/model_architecture.py
@@ -52,28 +52,79 @@ def build_discriminator64(image_size=(64,64),channels=64):
 
     return model
 
-def build_generator32():
+def build_generator64():
     model = tf.keras.Sequential()
-    model.add(layers.Dense(8*8*256, use_bias=False, input_shape=(100,)))
+    model.add(layers.Dense(4*4*1024, use_bias=False, input_shape=(100,)))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
-    model.add(layers.Reshape((8,8,256)))
+    model.add(layers.Reshape((4,4,1024)))
 
-    model.add(layers.Conv2DTranspose(128, 4, strides=(1, 1), padding='same', use_bias=False))
+    model.add(layers.Conv2DTranspose(512, 4, strides=(2, 2), padding='same', use_bias=False))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
-    model.add(layers.Conv2DTranspose(64, 4, strides=(2, 2), padding='same', use_bias=False))
+    model.add(layers.Conv2DTranspose(256, 4, strides=(2, 2), padding='same', use_bias=False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(128, 4, strides=(2, 2), padding='same', use_bias=False))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
     model.add(layers.Conv2DTranspose(3, 4, strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
     return model
 
-def build_discriminator32():
+def build_discriminator64():
     model = tf.keras.Sequential()
     model.add(layers.Conv2D(64, 3, strides=(2, 2), padding='same',
+                                     input_shape=(64,64,3)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2D(128, 3, strides=(2, 2), padding='same'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2D(256, 3, strides=(2, 2), padding='same'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2D(512, 3, strides=(2, 2), padding='same'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1))
+
+    return model
+    
+def build_generator32():
+    model = tf.keras.Sequential()
+    model.add(layers.Dense(4*4*1024, use_bias=False, input_shape=(100,)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Reshape((4,4,1024)))
+
+    model.add(layers.Conv2DTranspose(512, 4, strides=(2, 2), padding='same', use_bias=False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(256, 4, strides=(2, 2), padding='same', use_bias=False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(128, 4, strides=(2, 2), padding='same', use_bias=False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(3, 4, strides=(1, 1), padding='same', use_bias=False, activation='tanh'))
+    return model
+
+def build_discriminator32():
+    model = tf.keras.Sequential()
+    model.add(layers.Conv2D(64, 3, strides=(1, 1), padding='same',
                                      input_shape=(32,32,3)))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
@@ -86,6 +137,10 @@ def build_discriminator32():
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
+    model.add(layers.Conv2D(512, 3, strides=(2, 2), padding='same'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
     model.add(layers.Flatten())
     model.add(layers.Dense(1))
 
@@ -93,16 +148,17 @@ def build_discriminator32():
 
 def res_block_64(x,c,k_s,samp=None):
     if samp=='up':
-        shortcut=layers.Conv2DTranspose(c,k_s,strides=(2, 2),padding='same',use_bias=False)(x)
-        shortcut=layers.Conv2D(c,k_s,padding='same',use_bias=False)(shortcut)
+        c1=layers.Conv2DTranspose(c,k_s,strides=(2, 2),padding='same',use_bias=False)
+        c2=layers.Conv2D(c,k_s,padding='same',use_bias=False)
     elif samp=='down':
-        shortcut=layers.Conv2D(c,k_s,strides=(2, 2), padding='same',use_bias=False)(x)
-        shortcut=layers.Conv2D(c,k_s, padding='same',use_bias=False)(shortcut)
+        c1=layers.Conv2D(c,k_s,strides=(2, 2), padding='same',use_bias=False)
+        c2=layers.Conv2D(c,k_s, padding='same',use_bias=False)
     elif samp==None:
-        shortcut=layers.Conv2D(c,k_s, padding='same',use_bias=False)(x)
-        shortcut=layers.Conv2D(c,k_s, padding='same',use_bias=False)(shortcut)
+        c1=layers.Conv2D(c,k_s, padding='same',use_bias=False)
+        c2=layers.Conv2D(c,k_s, padding='same',use_bias=False)
 
-    return x + 
+    sho
+    return x
 
 def build_discriminator64_wgangp():
     model = tf.keras.Sequential()
