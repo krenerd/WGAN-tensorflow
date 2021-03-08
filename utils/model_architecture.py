d509fbf6554f52e01638d2ee8478d68f7a9a02c5
SieunPark <sieunpark77@gmail.com>
2021-03-08 09:13:18 +0900

    Update model_architecture.py

--- utils/model_architecture.py
+++ utils/model_architecture.py
@@ -1,57 +1,6 @@
 from tensorflow.keras import layers
 import tensorflow as tf
 
-def build_generator64(image_size=(64,64),channels=64):
-    w,h=image_size
-    
-    model = tf.keras.Sequential()
-    model.add(layers.Dense(w*h*channels//16, use_bias=False, input_shape=(100,)))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
-
-    model.add(layers.Reshape((w//8,h//8,channels*4)))
-
-    model.add(layers.Conv2DTranspose(channels*4, 4, strides=(1, 1), padding='same', use_bias=False))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
-
-    model.add(layers.Conv2DTranspose(channels*2, 4, strides=(2, 2), padding='same', use_bias=False))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
-
-    model.add(layers.Conv2DTranspose(channels, 4, strides=(2, 2), padding='same', use_bias=False))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
-    
-    model.add(layers.Conv2DTranspose(3, 4, strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
-    return model
-
-def build_discriminator64(image_size=(64,64),channels=64):
-    model = tf.keras.Sequential()
-    model.add(layers.Conv2D(channels, 4, strides=(2, 2), padding='same',
-                                     input_shape=(64,64,3)))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
-    #model.add(layers.Dropout(0.3))
-
-    model.add(layers.Conv2D(channels*2, 4, strides=(2, 2), padding='same'))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
-    #model.add(layers.Dropout(0.3))
-
-    model.add(layers.Conv2D(channels*4, 4, strides=(2, 2), padding='same'))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
-
-    model.add(layers.Conv2D(channels*8, 4, strides=(2, 2), padding='same'))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
-
-    model.add(layers.Flatten())
-    model.add(layers.Dense(1,activation='sigmoid'))
-
-    return model
-
 def build_generator64():
     model = tf.keras.Sequential()
     model.add(layers.Dense(4*4*1024, use_bias=False, input_shape=(100,)))
